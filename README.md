10291e05b0d80a5cbcc72ccc0d2b2332e7b7474b
Kristine Martin <kristinenicolemartin@gmail.com>
2017-11-19 12:42:31 -0800

    updated index, css, and js

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # TriviaGame
+
+## Are you a Silicon Valley nerd? 
+Test your knowledge by taking the ultimate SV quiz.
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,75 +1,30 @@
+// button onclick - start game
 
-var questions= [
-	"What was the first high level programming language developed for a computer?", 
-	"When was Plankalkül created?",
-	"When was JavaScript created?",
-	"Who was JavaScript created by?",
-	"What was JavaScript's original name?",
-	"Who coined the term Ajax?",
-	"When was jQuery originally released?",
-	"Who was the original author of jQuery?", 
-	"Which license is jQuery under?"];
+//start game - 
+	// start clock timer 30 seconds
+	// first question
+	// if timer runs out, next question
+	// if user guesses correctly, score ++
+	// if user guesses incorrectly, next question
 
-var choices: {["HTML", "Plankalkül", "CSS", "Java"],
-	["Between 1971 and 1973", "Between 1964 and 1966", "Between 1943 and 1945", "Between 1980 and 1982"],
-	["May 1995", "July 1993", "October 2001", "January 1996"],
-	["Brandon Eight", "Eric Brand", "Gordon Smith", "Brendan Eich"],
-	["Vanilla", "Mocha", "Carmel", "Java"],
-	["3 months", "10 days", "1 year", "10 years"],
-	["Jesse James Garrett", "John Paul Jefferson", "Sam Jacob Wills", "William Zach Cobb"],
-	["June 2002", "February 2004", "January 2006", "December 2009"],
-	["John Resig", "Phillip Knight", "David Walsh", "Ted Mosby"],
-	["Harvard", "Facebook", "Twitter", "MIT"],
-};
+//next question, restart timer & repeat 
 
-correctAnswer: [1,2,0,3,3,1,0,2,0,3];
+// when last question answered, display user score & giphy
 
-var userScore = 0;
-var userAnswer = [];
-var intervalId;
-var time = 30;
+$("#ready").click(startGame);
 
+function startGame() {
 
+}
 
-$(document).ready(function() {
-		confirm("Are you ready to play?");
+function triviaGame() {
 
+}
 
-		if (confirm) {
-			run();
-		};
+function endGame() {
 
-	function run() {
-		intervalId = setInterval(decrement, 1000);
-	};
+}
 
-	function decrement() {
-		time--;
-
-		$("#timer").html("<h1>" + time + "</h1>");
-
-		if (time === 0) {
-
-			stop();
-
-			nextQuestion();
-		};
-	}
-	function stop() {
-		clearInterval(intervalId);
-
-	}
-
-	// function nextQuestion() {
-	// 	run();
-
-	// 	$(".question").html("<h3>" + questions[i] + "</h3>")
-	// }
-
-// run();
-
-
-});
-
-
-// $(".answer").on("click", run);
+function startTimer() {
+	
+}

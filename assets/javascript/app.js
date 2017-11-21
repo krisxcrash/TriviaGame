8b435a608e4c396a6ad17785ad4fee7180b9635c
Kristine Martin <kristinenicolemartin@gmail.com>
2017-11-20 22:10:26 -0800

    updated javascript to add in timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,22 +11,49 @@
 
 // when last question answered, display user score & giphy
 
-$("#ready").click(startGame);
+var timerRunning;
 
 
+$(document).ready( function() {
+	timerRunning = false;
+});
+
+$("#ready-btn").click(function () {
+
+
+	var time = 31;
+	var timer = setInterval(startTimer, 1000);
+	startGame();
 
 function startGame() {
-
+	$("#ready-q").addClass("hidden");
+	$("#ready-btn").addClass("hidden");
+	startTimer();
+	triviaGame();
 }
 
 function triviaGame() {
+	$("#question-1").removeClass("hidden");
 
 }
 
-function endGame() {
+// function endGame() {
 
-}
+// }
 
 function startTimer() {
-
+	$("#timer").removeClass("hidden");
+	timerRunning = true;
+	if (timerRunning) {
+		time--;
+		if (time <= -1) {
+			clearInterval(timer);
+			// seeResults();
+			return;
+		}
+	}
+	document.getElementById("timer").innerHTML = time;
 }
+
+
+});

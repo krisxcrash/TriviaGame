a015932faec05c9996185e0f6945c06d5de7cf4c
Kristine Martin <kristinenicolemartin@gmail.com>
2017-11-23 12:59:17 -0800

    updated quiz html - changed from radios to buttons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,56 +12,7 @@
 // when last question answered, display user score & giphy
 
 var timerRunning;
-var questions = [{
-		question: "Who said, 'My username is 'password' and my password is 'password.'",
-		answers: ["Jared","Big Head", "Erlich", "Dinesh"],
-		correctAnswer: "Big Head"
-	},
-	{
-		question: "testing2",
-		answers: ["", "", "", ""],
-		correctAnswer: ""
-	},
-	{
-		question: "testing2",
-		answers: ["", "", "", ""],
-		correctAnswer: ""
-	},
-	{
-		question: "testing2",
-		answers: ["", "", "", ""],
-		correctAnswer: ""
-	},
-	{
-		question: "testing2",
-		answers: ["", "", "", ""],
-		correctAnswer: ""
-	},
-	{
-		question: "testing2",
-		answers: ["", "", "", ""],
-		correctAnswer: ""
-	},
-	{
-		question: "testing2",
-		answers: ["", "", "", ""],
-		correctAnswer: ""
-	},
-	{
-		question: "testing2",
-		answers: ["", "", "", ""],
-		correctAnswer: ""
-	},
-	{
-		question: "testing2",
-		answers: ["", "", "", ""],
-		correctAnswer: ""
-	},
-	{
-		question: "testing2",
-		answers: ["", "", "", ""],
-		correctAnswer: ""
-	}];
+var questions = ["question-1", "question-2", "question-3", "question-4", "question-5", "question-6", "question-7", "question-8", "question-9", "question-10"];
 
 
 
@@ -84,20 +35,22 @@ function startGame() {
 }
 
 function triviaGame() {
-	for (var i = 0; i < questions.length; i++) {
-		var q = questions[i].question;
-	$(".trivia-questions").removeClass("hidden");
+	$(".question-1").removeClass("hidden");
 		$("#question-div").innerHTML= q;
 	
 
 
 	}
 
+
+	function nextQuestion() {
+		for (var i = 0; i < questions.length; i++) {
+			var q = questions[i].question;
+		
+	}
 }
 
-// function endGame() {
 
-// }
 
 function startTimer() {
 	$("#timer").removeClass("hidden");
@@ -115,3 +68,21 @@ function startTimer() {
 
 
 });
+
+// the selector will match all input controls of type :checkbox
+// and attach a click event handler 
+$("input:checkbox").on('click', function() {
+	// in the handler, 'this' refers to the box clicked on
+	var $box = $(this);
+	if ($box.is(":checked")) {
+	  // the name of the box is retrieved using the .attr() method
+	  // as it is assumed and expected to be immutable
+	  var group = "input:checkbox[name='" + $box.attr("name") + "']";
+	  // the checked state of the group/box on the other hand will change
+	  // and the current value is retrieved using .prop() method
+	  $(group).prop("checked", false);
+	  $box.prop("checked", true);
+	} else {
+	  $box.prop("checked", false);
+	}
+  });

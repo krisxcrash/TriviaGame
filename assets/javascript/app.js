e52678192df5338334dfa9433ab35a9a921d9b3d
Kristine Martin <kristinenicolemartin@gmail.com>
2017-11-23 13:21:52 -0800

    updated quiz questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,6 +12,7 @@
 // when last question answered, display user score & giphy
 
 var timerRunning;
+var score = 0;
 var questions = ["question-1", "question-2", "question-3", "question-4", "question-5", "question-6", "question-7", "question-8", "question-9", "question-10"];
 
 
@@ -36,7 +37,6 @@ function startGame() {
 
 function triviaGame() {
 	$(".question-1").removeClass("hidden");
-		$("#question-div").innerHTML= q;
 	
 
 
@@ -46,7 +46,7 @@ function triviaGame() {
 	function nextQuestion() {
 		for (var i = 0; i < questions.length; i++) {
 			var q = questions[i].question;
-		
+			$("." + q).removeClass("hidden");
 	}
 }
 
@@ -68,21 +68,3 @@ function startTimer() {
 
 
 });
-
-// the selector will match all input controls of type :checkbox
-// and attach a click event handler 
-$("input:checkbox").on('click', function() {
-	// in the handler, 'this' refers to the box clicked on
-	var $box = $(this);
-	if ($box.is(":checked")) {
-	  // the name of the box is retrieved using the .attr() method
-	  // as it is assumed and expected to be immutable
-	  var group = "input:checkbox[name='" + $box.attr("name") + "']";
-	  // the checked state of the group/box on the other hand will change
-	  // and the current value is retrieved using .prop() method
-	  $(group).prop("checked", false);
-	  $box.prop("checked", true);
-	} else {
-	  $box.prop("checked", false);
-	}
-  });

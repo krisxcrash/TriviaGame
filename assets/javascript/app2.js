8f36692cfed1509bd634155334246801964cf45c
Kristine Martin <kristinenicolemartin@gmail.com>
2017-11-27 13:19:08 -0800

    updated results function

--- assets/javascript/app2.js
+++ assets/javascript/app2.js
@@ -133,7 +133,24 @@ function buildQuiz() {
 }
 
 function showResults() {
+	const answerContainers = quizContainer.querySelectorAll('.answers');
+		let numCorrect = 0;
 
+		myQuestions.forEach( (currentQuestion, questionNumber) => {
+			const answerContainer = answerContainers[questionNumber];
+			const selector = 'input[name=question'+questionNumber+']:checked';
+
+			const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+			if (userAnswer===currentQuestion.correctAnswer) {
+				numCorrect++;
+				answerContainers[questionNumber].style.color = 'lightgreen';
+			}
+			else {
+				answerContainers[questionNumber].style.color = 'red';
+			}
+		});
+		resultsContainer.innerHTML = numCorrect + ' out of ' + myQuestions.length;
 }
 
 //display Quiz

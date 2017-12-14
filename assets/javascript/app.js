636ccb4c550f19bbe3b9cf15068727227f186300
Kristine Martin <kristinenicolemartin@gmail.com>
2017-12-13 21:09:37 -0800

    updated js - finished quiz

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,59 +6,59 @@ var questions = [{
 	question: `Who said, "My username is 'password' and my password is 'password'."?`,
 	answers: ["Jared", "Big Head","Erlich","Dinesh"],
 	correctAnswer: "Big Head",
-	image: "https://gph.is/RZKzbT"
+	image: "https://media.giphy.com/media/mXuPwCWpd2WLV5WCSQ/giphy.gif"
 }, {
 	question: "What was the name of the application that Pied Piper sold to Hooli?",
 	answers: ["Pied Piper Chat","Piper Connect","Pied Messenger","Piper Chat"],
 	correctAnswer: "Piper Chat",
-	image: "https://gph.is/2pcLsCJ"
+	image: "https://media.giphy.com/media/3ohzdZSF5qNsDVyqek/giphy.gif"
 }, {
 	question: "Who created the SeeFood application, Not Hotdog?",
 	answers: ["Jin Yang","Erlich","Richard","Gilfoyle"],
 	correctAnswer: "Jin Yang",
-	image: "https://gph.is/2sJ8LEs"
+	image: "https://media.giphy.com/media/l0Iy9iqThC2ueLTkA/giphy.gif"
 }, {
 	question: "What was the name of Gilfoyle's server?",
 	answers: ["Antonio","Artemus","Anton","Android"],
 	correctAnswer: "Anton",
-	image: "https://gph.is/1Tyop8p"
+	image: "https://media.giphy.com/media/3oEjHAWsCGowjkc3Ha/giphy.gif"
 }, {
 	question: "What type of company did Keenan Feldspar have?",
 	answers: ["Virtual Reality","Augmented Reality","Mixed Reality","Artificial Intelligence"],
 	correctAnswer: "Virtual Reality",
-	image: "https://gph.is/2swZ6l0"
+	image: "https://media.giphy.com/media/l4FGkCHRY6x2Oy97q/giphy.gif"
 }, {
 	question: `Who said, "I'm not one to gush, but the possibilities of your consequence-free reality are darkly promising."?`,
 	answers: ["Gilfoyle","Erlich","Dinesh","Richard"],
 	correctAnswer: "Gilfoyle",
-	image: "https://gph.is/1HSQXV8"
+	image: "https://media.giphy.com/media/TMNvE4XgrGkzm/giphy.gif"
 }, {
 	question: "How much money did Peter Gregory offer students to drop out of college to pursue their dream?",
 	answers: ["$1,000,000","$250,000","$500,000","$100,000"],
-	correctAnswer: "d",
-	image: "https://gph.is/1H189tV"
+	correctAnswer: "$100,000",
+	image: "https://media.giphy.com/media/rWLFMDpUg3yV2/giphy.gif"
 }, {
 	question: "What was Jared's title while working for Pied Piper?",
 	answers: ["Chief Operating Officer","Chief Financial Officer","Head of Business Development","Business Consultant"],
 	correctAnswer: "Head of Business Development",
-	image: "https://gph.is/22C4xWL"
+	image: "https://media.giphy.com/media/xT1XGOGdyDrL2BTfxK/giphy.gif"
 }, {
 	question: "What school did Big Head start teaching at?",
 	answers: ["University of San Francisco","University of California, Berkeley","San Jose State University","Stanford University"],
 	correctAnswer: "Stanford University",
-	image: "https://gph.is/2pnj9wA"
+	image: "https://media.giphy.com/media/3ohzdS1YWu0zGYc5xe/giphy.gif"
 }, {
 	question: "What country did Gavin Belson travel to?",
 	answers: ["Tibet","Burma","Bhutan","Nepal"],
 	correctAnswer: "Tibet",
-	image: "https://gph.is/1yrDfcF"
+	image: "https://media.giphy.com/media/3oEdv7vedu6iAZoqTS/giphy.gif"
 }, ];
 
 var timer;
 
 var game = {
 	questions: questions,
-	currentQuestions: 0,
+	currentQuestion: 0,
 	counter: countStartNumber,
 	correct: 0,
 	incorrect: 0,
@@ -67,17 +67,16 @@ var game = {
 		this.counter--;
 		$("#counter-number").html(this.counter);
 		if (this.counter === 0) {
-			console.log("time's up!");
 			this.timeUp();
 		}
 	},
 	loadQuestion: function() {
 		timer = setInterval(this.countdown.bind(this), 1000);
 
-		panel.html("<h2 class='h2-q'>" + questions[this.currentQuestion].question + "</h2>");
+		panel.html("<h3 class='h3-q'>" + questions[this.currentQuestion].question + "</h3>");
 
-		for (var i = 0; questions[this.currentQuestion].answers.length; i++ ) {
-			panel.append("<button class='btn btn-lg btn-alert answer-button' id='button' data-name='" + questions[this.currentQuestion].answers[i]
+		for (var i = 0; i < questions[this.currentQuestion].answers.length; i++ ) {
+			panel.append("<button class='btn btn-lg btn-danger answer-button btn-answers' id='button' data-name='" + questions[this.currentQuestion].answers[i]
 			+ "'>" + questions[this.currentQuestion].answers[i] + "</button>");
 		}
 	},
@@ -91,27 +90,85 @@ var game = {
 		clearInterval(window.timer);
 		$("#counter-number").html(this.counter);
 
-		panel.html("<h2>Out of Time!</h2>");
-		panel.append("<h3>The Correct Answer wsa: " + questions[this.currentQuestion].correctAnswer);
-		panel.append("<img src='" + questions[this.currentQuestion].image + "' />");
+		panel.html("<h3>Out of Time!</h3>");
+		panel.append("<h3>The Correct Answer was: " + questions[this.currentQuestion].correctAnswer + "</h3>");
+		panel.append("<br><img src='" + questions[this.currentQuestion].image + "' />");
 
 		if (this.currentQuestion === questions.length -1) {
 			setTimeout(this.results, 3 * 1000);
 		}
 		else {
-			setTimeout(this.nextQuestion, 3 * 1000);
+			setTimeout(this.nextQuestion, 5 * 1000);
 		}
 	},
 	results: function() {
 		clearInterval(window.timer);
-		panel.html("<h2>Here's how you did! </h2>");
+		panel.html("<h3>Here's how you did! </h3>");
 
 		$("#counter-number").html(this.counter);
 
-		panel.append("<h3>Correct Answers: " + this.correct + "</h3>");
-		panel.append("<h3> Incorrect Answers: " + this.incorrect + "</h3>");
-		panel.append("<h3>Unanswered: " + (questions.length - (this.incorrect + this.correct)) + "</h3>");
-		panel.append("<br><button id='start-over'>Start Over</button>");
+		panel.append("<h4>Correct Answers: " + this.correct + "</h4>");
+		panel.append("<h4> Incorrect Answers: " + this.incorrect + "</h4>");
+		panel.append("<h4>Unanswered: " + (questions.length - (this.incorrect + this.correct)) + "</h4>");
+		panel.append("<br><button class='btn btn-danger btn-lg' id='start-over'>Start Over</button>");
 	},
-	
-}
+	clicked: function(e) {
+		clearInterval(window.timer);
+
+		if ($(e.target).attr("data-name") === questions[this.currentQuestion].correctAnswer) {
+			this.answeredCorrectly();
+		}
+		else {
+			this.answeredIncorrectly();
+		}
+	},
+
+	answeredIncorrectly: function() {
+		this.incorrect++;
+
+		clearInterval(window.timer);
+
+		panel.html("<h3>Bummer! That is not correct!</h3>");
+		panel.html("<h3>The correct answer is: " + questions[this.currentQuestion].correctAnswer + "</h3>");
+		panel.append("<br><img src='" + questions[this.currentQuestion].image + "' alt= 'giphy'/>");
+
+		if (this.currentQuestion === questions.length - 1) {
+			setTimeout(this.results.bind(this), 3 * 1000);
+		}
+		else {
+			setTimeout(this.nextQuestion.bind(this), 3 * 1000);
+		}
+	},
+	answeredCorrectly: function() {
+		clearInterval(window.timer);
+
+		this.correct++;
+
+		panel.html("<h3>Nailed it!</h3>");
+		panel.append("<br><img src='" + questions[this.currentQuestion].image + "' />");
+
+		if (this.currentQuestion === questions.length - 1) {
+			setTimeout(this.results.bind(this), 3 * 1000);
+		}
+		else {
+			setTimeout(this.nextQuestion.bind(this), 3 * 1000);
+		}
+	},
+	reset: function() {
+		this.currentQuestion = 0;
+		this.counter = 0;
+		this.correct = 0;
+		this.incorrect = 0;
+		this.loadQuestion();
+	}
+};
+
+$(document).on("click", "#start-over", game.reset.bind(game));
+$(document).on("click", ".answer-button", function(e) {
+	game.clicked.bind(game, e)();
+});
+$(document).on("click", "#start", function() {
+	$("#ready").addClass("hidden");
+	$("#sub-wrapper").prepend("<h1 id='timer'><span id='counter-number'>30</span></h1>");
+	game.loadQuestion.bind(game)();
+})
